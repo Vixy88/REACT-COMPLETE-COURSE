74c76b79782b2ae230465716353e9b5a4db66726
Michael Jakobsen <michael@rpair.co.uk>
2022-01-30 18:06:03 +0000

    added new header component to build header and navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import ExpenseItem from "./components/ExpenseItem";
+import DisplayHeader from "./components/Header";
 
 function App() {
   return (
     <div>
+      <DisplayHeader></DisplayHeader>
       <h2>Let's get started!!!</h2>
       <ExpenseItem></ExpenseItem>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function DisplayHeader() {
+  return (
+    <div>
+      <nav>
+        <h1>LOGO</h1>
+        <ul>
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+          <li>Link 4</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default DisplayHeader;
