7ebc80ac05ee5ab22acae6cca8bbe2cc785aa048
Michael Jakobsen <michael@rpair.co.uk>
2022-01-31 20:56:38 +0000

    updated expenseitem component to use props

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       id: "e1",
       title: "Car Insurance",
       amount: 294.67,
-      date: new DataTransfer(2021, 1, 15),
+      date: new Date(2021, 1, 15),
     },
     {
       id: "e2",
       title: "Dog Insurance",
       amount: 60,
-      date: new DataTransfer(2021, 1, 28),
+      date: new Date(2021, 1, 28),
     },
     {
       id: "e3",
       title: "Mobile Phone",
       amount: 22,
-      date: new DataTransfer(2021, 2, 08),
+      date: new Date(2021, 2, 8),
     },
     {
       id: "e4",
       title: "Entertainment",
       amount: 5000,
-      date: new DataTransfer(2021, 3, 28),
+      date: new Date(2021, 3, 28),
     },
   ];
 
@@ -33,7 +33,26 @@ function App() {
     <div>
       <DisplayHeader></DisplayHeader>
       <h2>Let's get started!!!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
