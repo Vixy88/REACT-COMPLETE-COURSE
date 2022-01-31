cf2c23494e39946220e436627e13109d081df922
Michael Jakobsen <michael@rpair.co.uk>
2022-01-31 21:23:01 +0000

    added styling to the header and a test function to test the button functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
-import React from "react";
+import "./Header.css";
 
 function DisplayHeader() {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <div>
       <nav>
         <h1>LOGO</h1>
         <ul>
-          <li>Link 1</li>
-          <li>Link 2</li>
-          <li>Link 3</li>
-          <li>Link 4</li>
+          <li>
+            <button>About</button>
+          </li>
+          <li>
+            <button onClick={sayHello}>How to use</button>
+          </li>
+          <li>
+            <button>Contact</button>
+          </li>
         </ul>
       </nav>
     </div>
