e2bc684a5438000600169377eb1fdb011a111dd1
Michael Jakobsen <michael@rpair.co.uk>
2022-01-31 20:38:50 +0000

    added variable with an array storing 4 example expenses in it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import ExpenseItem from "./components/ExpenseItem";
 import DisplayHeader from "./components/Header";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new DataTransfer(2021, 1, 15),
+    },
+    {
+      id: "e2",
+      title: "Dog Insurance",
+      amount: 60,
+      date: new DataTransfer(2021, 1, 28),
+    },
+    {
+      id: "e3",
+      title: "Mobile Phone",
+      amount: 22,
+      date: new DataTransfer(2021, 2, 08),
+    },
+    {
+      id: "e4",
+      title: "Entertainment",
+      amount: 5000,
+      date: new DataTransfer(2021, 3, 28),
+    },
+  ];
+
   return (
     <div>
       <DisplayHeader></DisplayHeader>
